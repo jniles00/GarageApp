75f04ac3d0c3bc42776c474d58ec256f45b0548f
jniles00 <97948400+jniles00@users.noreply.github.com>
2022-02-22 12:25:50 +0000

    Add files via upload

--- Garage/src/Garage/Garage.java
+++ Garage/src/Garage/Garage.java
@@ -5,24 +5,24 @@ import vehicles.*;
 
 public class Garage {
 
-	public ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
+	public ArrayList<Vehicle> vehiclesArr = new ArrayList<Vehicle>();
 
 	public void addVehicle(Vehicle vehicle) {
 		// this.addVehicle(vehicle);
-		this.vehicles.add(vehicle);
+		this.vehiclesArr.add(vehicle);
 	}
 	
 	public void removeVehicle(Vehicle vehicle)
 	{
-		this.vehicles.remove(vehicle);
+		this.vehiclesArr.remove(vehicle);
 	}
 	
 	
 	public void removeVehicleByID(int i)
 	{
 		Vehicle remove = null;
-		for(Vehicle vehicles : vehicles)
-		{			
+		for(Vehicle vehicles : vehiclesArr)
+		{//DataType| Item		Array	
 			if (vehicles.getID() == i)
 			{
 				remove = vehicles;
@@ -34,7 +34,7 @@ public class Garage {
 	public void removeVehicleByManu(String s)
 	{
 		Vehicle remove = null;
-		for(Vehicle vehicles : this.vehicles)
+		for(Vehicle vehicles : this.vehiclesArr)
 		{			
 			if (vehicles.getManufacturer() == s)
 			{
@@ -44,32 +44,44 @@ public class Garage {
 		}
 	}
 	
-	// Array method
-//	public void removeVehicleByManu(int i)
-//	{
-//		ArrayList<Vehicle> remove = new ArrayList<>();
-//		for(Vehicle vehicle : vehicles)
-//		{
-//			if(vehicle.getID() == i)
-//			{
-//				remove.add(vehicle);
-//			}
-//		}
-//		remove.remove(i);
-//	}
+	public void emptyGarage()
+	{
+		this.vehiclesArr.clear();
+	}
+	
+	 // remove multiple vehicles by manufacturer
+	public void removeVehiclesByManu(String s)
+	{
+		ArrayList<Vehicle> removeArr = new ArrayList<>();
+		for(Vehicle vehicle : vehiclesArr)
+		{
+			if(vehicle.getManufacturer() == s)
+			{
+				removeArr.add(vehicle);
+			}
+		}
+		
+		for(Vehicle destroy : removeArr)
+		{
+			vehiclesArr.remove(destroy);
+		}
+	}
 	
 	public int fixVehicleByID(int i)
 	{
 		int fix = 0;
-		for(Vehicle vehicles : this.vehicles)
+		
+		for(Vehicle vehicles : this.vehiclesArr)
 		{
+
 			fix = vehicles.getPrice();
+
 		}
-		return fix = fix / 2;
+		return fix = fix / 2;	
 	}
 	
 	public void printVehicles() {
-		for (Vehicle vehicle : this.vehicles) {
+		for (Vehicle vehicle : this.vehiclesArr) {
 			System.out.println(vehicle);
 		}
 	}
--- Garage/src/runner/Runner.java
+++ Garage/src/runner/Runner.java
@@ -10,31 +10,41 @@ public class Runner {
 		Car carOne = new Car(1, "Ford", "Red", 4, true, false, 4, false, 200);
 		Motorbike bikeOne = new Motorbike(2, "Hayabusa", "Yellow", 2, true, false, 10, 500);
 		Van vanOne = new Van(3, "Big Man Van", "White", 4, true, false, 10, 350);
-//		Car carTwo = new Car(4, "Honda", "Blue", 6, false, true, 6, false, 900);
+		Car carTwo = new Car(4, "Honda", "Blue", 6, false, true, 6, false, 900);
+		Motorbike bikeTwo = new Motorbike(5, "Ford", "Orange", 2, false, true, 5, 1000);
 		
 		Garage garage = new Garage();
 		garage.addVehicle(carOne);
 		garage.addVehicle(bikeOne);
 		garage.addVehicle(vanOne);
-//		garage.addVehicle(carTwo);
+		garage.addVehicle(carTwo);
+		garage.addVehicle(bikeTwo);
 		
 		System.out.println("========All Vehicles============");
 		garage.printVehicles();
 		
-		System.out.println("=========Remove Vehicle==========");
-		garage.removeVehicle(vanOne);		
-		garage.printVehicles();
+//		System.out.println("\n=========Remove Vehicle==========");
+//		garage.removeVehicle(vanOne);		
+//		garage.printVehicles();
+//		
+//		System.out.println("\n========Remove by ID==============");
+//		garage.removeVehicleByID(2);
+//		garage.printVehicles();
+//		
+//		System.out.println("\n====Remove by Manufacturer=====");
+//		garage.removeVehicleByManu("Ford");
+//		garage.printVehicles();
+//		
+//		System.out.println("\n========Fix by Price==========");
+//		System.out.println(garage.fixVehicleByID(4));
+//		garage.printVehicles();
+//		
+//		System.out.println("\n========Empty Garage==========");
+//		garage.emptyGarage();
+//		garage.printVehicles();
 		
-		System.out.println("========Remove by ID==============");
-		garage.removeVehicleByID(1);
-		garage.printVehicles();
-		
-		System.out.println("====Remove by Manufacturer=====");
-		garage.removeVehicleByManu("Ford");
-		garage.printVehicles();
-		
-		System.out.println("========Fix by Price==========");
-		System.out.println(garage.fixVehicleByID(4));
+		System.out.println("\n===Remove multiple by Manufacturer===");
+		garage.removeVehiclesByManu("Ford");
 		garage.printVehicles();
 
 	}	
